91824996aee22652030df050bf9505b080c642d7
HamzaShaukat123 <162437543+HamzaShaukat123@users.noreply.github.com>
2024-12-10 14:13:41 +0500

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2601,7 +2601,7 @@
 						salesRows += `<tr>
 							<td>${value['ac_name'] ? value['ac_name'] : ''}</td>
 							<td>${value['sale_prefix'] ? value['sale_prefix'] : ''} ${value['Sal_inv_no'] ? value['Sal_inv_no'] : ''}</td>
-							<td>${value['bill_date'] ? value['bill_date'] : ''}</td>
+							<td class="text-center">${value['bill_date'] ? moment(value['bill_date']).format('D-M-YY') : ''}</td>
 							<td>${value['bill_amount'] ? value['bill_amount'] : ''}</td>
 							<td>${value['ttl_jv_amt'] ? value['ttl_jv_amt'] : ''}</td>
 							<td>${value['remaining_amount'] ? value['remaining_amount'] : ''}</td>
