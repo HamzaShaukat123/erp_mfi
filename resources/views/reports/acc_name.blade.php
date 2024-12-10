ba49431939ff1c31baa04912054a48cbe9a4ef02
HamzaShaukat123 <162437543+HamzaShaukat123@users.noreply.github.com>
2024-12-10 12:03:02 +0500

    Update acc_name.blade.php

--- resources/views/reports/acc_name.blade.php
+++ resources/views/reports/acc_name.blade.php
@@ -908,7 +908,7 @@
                             <td>${v['21_35_Days'] ? v['21_35_Days'] : ''}</td>
                             <td>${v['36_50_Days'] ? v['36_50_Days'] : ''}</td>
                             <td>${v['over_50_Days'] ? v['over_50_Days'] : ''}</td>
-                            <td style="${maxDaysStyle}">${v['max_days'] ? v['max_days'] : ''}</td>
+                            <td style="${remainingAmount === 0 ? maxDaysStyle : ''}">${remainingAmount === 0 ? (v['max_days'] ? v['max_days'] : '') : ''}</td>
                             <td>${remainingAmount === 0 ? 'Cleared' : 'Not Cleared'}</td>
                         </tr>`;
                     });
