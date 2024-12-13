91be0c40268a1e38bd821567d7ffc85e71277f16
HamzaShaukat123 <162437543+HamzaShaukat123@users.noreply.github.com>
2024-12-13 12:09:43 +0500

    Update RptItemGroupSale1Controller.php

--- app/Http/Controllers/RptItemGroupSale1Controller.php
+++ app/Http/Controllers/RptItemGroupSale1Controller.php
@@ -55,7 +55,7 @@ class RptItemGroupSale1Controller extends Controller
         $pdf = new MyPDF();
         $pdf->SetCreator(PDF_CREATOR);
         $pdf->SetAuthor('MFI');
-        $pdf->SetTitle('Sales Report Of Item Group - ' . $sale_account_item_group_info[0]['item_group_code']);
+        $pdf->SetTitle('Sales Report Of Item Group - ' . $sale_account_item_group_info[0]['item_group_name']);
         $pdf->SetSubject('Sales Report');
         $pdf->SetKeywords('Sales Report, TCPDF, PDF');
         $pdf->setPageOrientation('P');
@@ -73,7 +73,7 @@ class RptItemGroupSale1Controller extends Controller
         <table style="border:1px solid #000; width:100%; padding:6px; border-collapse:collapse;">
             <tr>
                 <td style="font-size:12px; font-weight:bold; color:#17365D; border-bottom:1px solid #000; width:70%;">
-                    Item Group: <span style="color:black;">' . $sale_account_item_group_info[0]['item_group_code'] . '</span>
+                    Item Group: <span style="color:black;">' . $sale_account_item_group_info[0]['item_group_name'] . '</span>
                 </td>
                 <td style="font-size:12px; font-weight:bold; color:#17365D; text-align:left; border-bottom:1px solid #000;border-left:1px solid #000; width:30%;">
                     Print Date: <span style="color:black;">' . $formattedDate . '</span>
