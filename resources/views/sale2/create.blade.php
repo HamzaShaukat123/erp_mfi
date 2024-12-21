e536be3db73915ace582f72e3f9a47163398f468
HamzaShaukat123 <162437543+HamzaShaukat123@users.noreply.github.com>
2024-12-21 17:24:00 +0500

    w

--- resources/views/sale2/create.blade.php
+++ resources/views/sale2/create.blade.php
@@ -322,20 +322,6 @@
 				// toggleInputs();
 			});
 
-			// function removeRow(button) {
-			// 	var tableRows = $("#Purchase2Table tr").length;
-			// 	if(tableRows>1){
-			// 		var row = button.parentNode.parentNode;
-			// 		row.parentNode.removeChild(row);
-			// 		index--;	
-			// 		itemCount = Number($('#itemCount').val());
-			// 		itemCount = itemCount-1;
-			// 		$('#itemCount').val(itemCount);
-			// 	}  
-			// 	tableTotal(); 
-			// }
-
-
 
 			function removeRow(button) {
 				if (confirm("Are you sure you want to remove this row?")) {
