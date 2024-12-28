157c7e34ac240aacc47c3aafe2fe7cbd2fcc9cca
HamzaShaukat123 <162437543+HamzaShaukat123@users.noreply.github.com>
2024-12-28 15:56:50 +0500

    Update RptGoDownItemGroupController.php

--- app/Http/Controllers/RptGoDownItemGroupController.php
+++ app/Http/Controllers/RptGoDownItemGroupController.php
@@ -39,6 +39,7 @@ class RptGoDownItemGroupController extends Controller
         // ->get();
 
         $pipe_stock_all_by_item_group = pipe_stock_all_by_item_group::where('pipe_stock_all_by_item_group.item_group_cod', $request->acc_id)
+        ->where('pipe_stock_all_by_item_group.opp_bal', '<>', 0)
         ->leftJoin('item_group', 'item_group.item_group_cod', '=', 'pipe_stock_all_by_item_group.item_group_cod')
         ->select(
             'pipe_stock_all_by_item_group.item_group_cod',
