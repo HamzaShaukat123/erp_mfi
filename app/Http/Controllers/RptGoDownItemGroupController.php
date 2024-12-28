2a12661b739c08a0037ad6be262c346796d299c0
HamzaShaukat123 <162437543+HamzaShaukat123@users.noreply.github.com>
2024-12-28 16:14:30 +0500

    Update RptGoDownItemGroupController.php

--- app/Http/Controllers/RptGoDownItemGroupController.php
+++ app/Http/Controllers/RptGoDownItemGroupController.php
@@ -85,7 +85,7 @@ class RptGoDownItemGroupController extends Controller
 
         // Header details
         $html = '
-        <table style="border:1px solid #000; width:100%; padding:6px; border-collapse:collapse;">
+        <table style="border:1px solid #000; width:100%; padding:4px; border-collapse:collapse;">
             <tr>
                 <td style="font-size:12px; font-weight:bold; color:#17365D; border-bottom:1px solid #000; width:100%;">
                     Item Group: <span style="color:black;">' . $pipe_stock_all_by_item_group[0]['item_group_cod'] . ' - ' . $pipe_stock_all_by_item_group[0]['group_name'] . '</span>
