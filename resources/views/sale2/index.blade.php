e7292096e09b8f61dee18f022298cb22fb2f55b9
HamzaShaukat123 <162437543+HamzaShaukat123@users.noreply.github.com>
2025-01-03 16:27:09 +0500

    s

--- resources/views/sale2/index.blade.php
+++ resources/views/sale2/index.blade.php
@@ -102,8 +102,10 @@
                                                         <a href="{{ route('show-sales2',$row->Sal_inv_no) }}" class=""><i class="fas fa-eye"></i></a>
                                                         <span class="separator"> | </span>
                                                         <a href="{{ route('edit-sales2',$row->Sal_inv_no) }}" class=""><i class="fas fa-pencil-alt"></i></a>
+                                                        @if(session('user_role')==1)
                                                         <span class="separator"> | </span>
                                                         <a class="mb-1 mt-1 me-1 modal-with-zoom-anim ws-normal" onclick="setId({{$row->Sal_inv_no}})" href="#deleteModal"><i class="far fa-trash-alt" style="color:red"></i></a>
+                                                        @endif
                                                     </td>
                                                 </tr>
                                                 @endforeach
--- resources/views/sales/index.blade.php
+++ resources/views/sales/index.blade.php
@@ -99,10 +99,12 @@
                                                         <a href="{{ route('edit-sale-invoice',$row->Sal_inv_no) }}" class="">
                                                             <i class="fas fa-pencil-alt"></i>
                                                         </a>
+                                                        @if(session('user_role')==1)
                                                         <span class="separator"> | </span>
                                                         <a class="mb-1 mt-1 me-1 modal-with-zoom-anim ws-normal" onclick="setId({{$row->Sal_inv_no}})" href="#deleteModal">
                                                             <i class="far fa-trash-alt" style="color:red"></i>
                                                         </a>
+                                                        @endif
                                                     </td>
 
                                                 </tr>
