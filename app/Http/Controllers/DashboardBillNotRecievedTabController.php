d9d6bc4347c56fe2b422fd37274d159539884f6f
HamzaShaukat123 <162437543+HamzaShaukat123@users.noreply.github.com>
2025-01-04 15:43:13 +0500

    Update DashboardBillNotRecievedTabController.php

--- app/Http/Controllers/DashboardBillNotRecievedTabController.php
+++ app/Http/Controllers/DashboardBillNotRecievedTabController.php
@@ -17,12 +17,18 @@ class DashboardBillNotRecievedTabController extends Controller
             'bill_not_recvd.bill_amount',
             'bill_not_recvd.ttl_jv_amt',
             'bill_not_recvd.remaining_amount',
-            'sales.pur_ord_no',
-            'sales.Cash_pur_name'
+            'sales.pur_ord_no', 
+            'sales.Cash_pur_name',
+            'tsales.Cash_name',
+            'tsales.pur_ord_no'
         )
         ->leftJoin('sales', function($join) {
             $join->on('bill_not_recvd.sale_prefix', '=', 'sales.prefix')
                  ->on('bill_not_recvd.Sal_inv_no', '=', 'sales.Sal_inv_no');
+        })
+        ->leftJoin('tsales', function($join) {
+            $join->on('bill_not_recvd.sale_prefix', '=', 'tsales.prefix')
+                 ->on('bill_not_recvd.Sal_inv_no', '=', 'tsales.Sal_inv_no');
         })
         ->where('bill_not_recvd.remaining_amount', '<>', 0)
         ->where('bill_not_recvd.account_name', '=', 19)
