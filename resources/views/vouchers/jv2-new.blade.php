838e03851f8aa8ca0744618caab73750e79b9c13
HamzaShaukat123 <162437543+HamzaShaukat123@users.noreply.github.com>
2025-01-04 18:39:48 +0500

    Update jv2-new.blade.php

--- resources/views/vouchers/jv2-new.blade.php
+++ resources/views/vouchers/jv2-new.blade.php
@@ -133,17 +133,17 @@
 
 											</div>
 
-											<div class="col-4 mb-2">
+											<div class="col-3 mb-2">
 												<label class="col-form-label">Unadjusted Amount</label>
 												<input type="number" id="sales_unadjusted_amount" name="sales_unadjusted_amount" value="0" class="form-control" disabled step="any">
 											</div>
 
-											<div class="col-4 mb-1">
+											<div class="col-3 mb-2">
 												<label class="col-form-label">Total Amount</label>
 												<input type="number" id="total_reci_amount" class="form-control" value="0" disabled step="any">
 											</div>
 
-											<div class="col-4 mb-1">
+											<div class="col-3 mb-2">
 												<label class="col-form-label">Remaining Amount</label>
 												<input type="number" id="total_rem_amount" class="form-control" value="0" disabled step="any">
 											</div>
