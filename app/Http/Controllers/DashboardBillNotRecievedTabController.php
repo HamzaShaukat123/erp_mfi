4b6cd89e327073d68eca2e5112a379b456e08d72
HamzaShaukat123 <162437543+HamzaShaukat123@users.noreply.github.com>
2025-01-04 15:27:18 +0500

    Update DashboardBillNotRecievedTabController.php

--- app/Http/Controllers/DashboardBillNotRecievedTabController.php
+++ app/Http/Controllers/DashboardBillNotRecievedTabController.php
@@ -11,6 +11,8 @@ class DashboardBillNotRecievedTabController extends Controller
     public function BillNotRecvd(Request $request)
     {
         $bill_not_recvd = bill_not_recvd::where('account_name', '=', '19')
+        ->leftJoin('sales as sales_prefix', 'bill_not_recvd.sale_prefix', '=', 'sales_prefix.prefix')
+        ->leftJoin('sales as sales_inv', 'bill_not_recvd.Sal_inv_no', '=', 'sales_inv.Sal_inv_no')
         ->where('remaining_amount', '<>', 0)
         ->get();
 
