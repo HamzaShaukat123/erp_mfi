d0539482446d6ac448787bcc2aea07b0bd4ce440
HamzaShaukat123 <162437543+HamzaShaukat123@users.noreply.github.com>
2025-01-04 15:30:35 +0500

    Update DashboardBillNotRecievedTabController.php

--- app/Http/Controllers/DashboardBillNotRecievedTabController.php
+++ app/Http/Controllers/DashboardBillNotRecievedTabController.php
@@ -11,10 +11,11 @@ class DashboardBillNotRecievedTabController extends Controller
     public function BillNotRecvd(Request $request)
     {
         $bill_not_recvd = bill_not_recvd::where('bill_not_recvd.account_name', '=', '19')
-        ->leftJoin('sales as sales_prefix', 'bill_not_recvd.sale_prefix', '=', 'sales_prefix.prefix')
-        ->leftJoin('sales as sales_inv', 'bill_not_recvd.Sal_inv_no', '=', 'sales_inv.Sal_inv_no')
-        ->where('bill_not_recvd.remaining_amount', '<>', 0)
-        ->get();
+    ->leftJoin('sales as sales_prefix', 'bill_not_recvd.sale_prefix', '=', 'sales_prefix.prefix')
+    ->leftJoin('sales as sales_inv', 'bill_not_recvd.Sal_inv_no', '=', 'sales_inv.Sal_inv_no')
+    ->where('bill_not_recvd.remaining_amount', '<>', 0)
+    ->groupBy('bill_not_recvd.id') // Replace 'id' with the unique field for `bill_not_recvd`
+    ->get();
 
 
 
