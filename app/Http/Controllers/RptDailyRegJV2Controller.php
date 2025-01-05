104a9a94f034381fb0508105a0acf728f440a5d4
HamzaShaukat123 <162437543+HamzaShaukat123@users.noreply.github.com>
2025-01-05 18:59:41 +0500

    Update RptDailyRegJV2Controller.php

--- app/Http/Controllers/RptDailyRegJV2Controller.php
+++ app/Http/Controllers/RptDailyRegJV2Controller.php
@@ -14,6 +14,8 @@ class RptDailyRegJV2Controller extends Controller
 {
     public function jv2(Request $request){
         $activites9_gen_acas = activites9_gen_acas::whereBetween('jv_date', [$request->fromDate, $request->toDate])
+        ->orderBy('jv_date')
+        ->orderBy('jv_no')
         ->get();
 
         return $activites9_gen_acas;
