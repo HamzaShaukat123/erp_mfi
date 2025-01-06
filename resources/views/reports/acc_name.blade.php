0b7321e6fd91ddc0822986f2a513ec46c36833ad
HamzaShaukat123 <162437543+HamzaShaukat123@users.noreply.github.com>
2025-01-06 13:36:21 +0500

    Update acc_name.blade.php

--- resources/views/reports/acc_name.blade.php
+++ resources/views/reports/acc_name.blade.php
@@ -1103,14 +1103,15 @@
                             html += "<td>" + (v['ac_name'] ? v['ac_name'] : "") + "</td>";
                             html += "<td>" + (v['pur_no'] ? v['pur_no'] : "") + "</td>";
                             html += "<td>" + (v['remarks'] ? v['remarks'] : "") + "</td>";
-                            html += "<td>" + (crAmt ? crAmt.toFixed(0) : "") + "</td>";
+                            html += "<td>" + (crAmt ? crAmt.toLocaleString('en-US', { minimumFractionDigits: 0, maximumFractionDigits: 0 }) : "") + "</td>";
                             html +="</tr>";
                             $(tableID).append(html);
                         });
 
                         // Display the total in the last row or specific cell
                         var totalRow = "<tr><td colspan='7' style='text-align: right;'><strong>Total:</strong></td>";
-                        totalRow += "<td class='text-danger'><strong>" + totalCrAmt.toFixed(0) + "</strong></td></tr>";
+                            totalRow += "<td class='text-danger'><strong>" + (totalCrAmt ? totalCrAmt.toLocaleString('en-US', { minimumFractionDigits: 0, maximumFractionDigits: 0 }) : "0") + "</strong></td></tr>";
+
                         $(tableID).append(totalRow);
                     },
                     error: function(){
