1dc0b722c8005a488329c2a895ab2ab472147d79
HamzaShaukat123 <162437543+HamzaShaukat123@users.noreply.github.com>
2025-01-07 16:22:41 +0500

    Update DashboardOverDaysTabController.php

--- app/Http/Controllers/DashboardOverDaysTabController.php
+++ app/Http/Controllers/DashboardOverDaysTabController.php
@@ -12,11 +12,13 @@ class DashboardOverDaysTabController extends Controller
     public function OverDays(Request $request)
     {
         $dash_over_days_sales = dash_over_days_sales::where('remaining_amount', '!=', 0)
+        ->where('AccountType', '=', 1)
         ->where('days_cross', '>', 0)
         ->orderBy('days_cross', 'desc')
         ->get();
 
         $dash_over_days_pur = dash_over_days_pur::where('remaining_amount', '!=', 0)
+        ->where('AccountType', '=', 7)
         ->where('days_cross', '>', 0)
         ->orderBy('days_cross', 'desc')
         ->get();
