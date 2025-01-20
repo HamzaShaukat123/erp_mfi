0a8d5cfd2914d2ebf66ff999ed3dec66a694fae2
HamzaShaukat123 <162437543+HamzaShaukat123@users.noreply.github.com>
2025-01-20 16:15:10 +0500

    Update RptAccNamePurAgeingController.php

--- app/Http/Controllers/RptAccNamePurAgeingController.php
+++ app/Http/Controllers/RptAccNamePurAgeingController.php
@@ -172,21 +172,23 @@ class RptAccNamePurAgeingController extends Controller
                     : '';
             
                 $html .= '<tr style="background-color:' . $bgColor . ';">
-                            <td>' . $count . '</td>
-                            <td>' . Carbon::createFromFormat('Y-m-d', $items['bill_date'])->format('d-m-y') . '</td>
-                            <td>' . htmlspecialchars($items["sale_prefix"] . $items["Sal_inv_no"]) . '</td>
-                            <td>' . htmlspecialchars($items["ac2"] . $items["remarks"]) . '</td>
-                            <td>' . number_format($items['bill_amount'], 0) . '</td>
-                            <td>' . number_format($items['remaining_amount'], 0) . '</td>
-                            <td>' . ($items['remaining_amount'] != 0 ? $daysFromBillDate : '') . '</td>
-                            <td>' . number_format($items['1_20_Days'], 0) . '</td>
-                            <td>' . number_format($items['21_35_Days'], 0) . '</td>
-                            <td>' . number_format($items['36_50_Days'], 0) . '</td>
-                            <td>' . number_format($items['over_50_Days'], 0) . '</td>
-                            <td ' . ($items['remaining_amount'] != 0 ? $maxDaysStyle : '') . '>' . 
+                           
+                            <td style="width:4%;">' . $count . '</td>
+                            <td style="width:9%;">' . Carbon::createFromFormat('Y-m-d', $items['bill_date'])->format('d-m-y') . '</td>
+                            <td style="width:8%;">' . htmlspecialchars($items["sale_prefix"] . $items["Sal_inv_no"]) . '</td>
+                            <td style="width:14%;">' . htmlspecialchars($items["ac2"] . $items["remarks"]) . '</td>
+                            <td style="width:10%;">' . number_format($items['bill_amount'], 0) . '</td>
+                            <td style="width:10%;">' . number_format($items['remaining_amount'], 0) . '</td>
+                            <td style="width:5%;">' . ($items['remaining_amount'] != 0 ? $daysFromBillDate : '') . '</td>
+                            <td style="width:8%;">' . number_format($items['1_20_Days'], 0) . '</td>
+                            <td style="width:8%;">' . number_format($items['21_35_Days'], 0) . '</td>
+                            <td style="width:8%;">' . number_format($items['36_50_Days'], 0) . '</td>
+                            <td style="width:8%;">' . number_format($items['over_50_Days'], 0) . '</td>
+                            <td style="width:8%;" ' . ($items['remaining_amount'] != 0 ? $maxDaysStyle : '') . '>' . 
                                 ($items['remaining_amount'] == 0 ? $items['max_days'] : '') . 
                                 ' - ' . $status . 
                             '</td>
+
                         </tr>';
             
                 $count++;
