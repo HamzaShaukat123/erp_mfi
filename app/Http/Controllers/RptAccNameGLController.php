5dcfeb12099bf92c04dad73d93b3726d32272ba1
HamzaShaukat123 <162437543+HamzaShaukat123@users.noreply.github.com>
2025-01-24 16:26:04 +0500

    Update RptAccNameGLController.php

--- app/Http/Controllers/RptAccNameGLController.php
+++ app/Http/Controllers/RptAccNameGLController.php
@@ -757,8 +757,8 @@ class RptAccNameGLController extends Controller
 
             // Add the totals row
             $html .= '
-            <tr style="font-weight:bold;">
-                <td colspan="6" style="background-color:#d9edf7; font-weight:bold; text-align:right;">Total</td>
+            <tr style="style="background-color:#d9edf7; font-weight:bold;">
+                <td colspan="6" style="text-align:right;">Total</td>
                 <td style="padding:10px; text-align:center;">' . number_format($totalDebit, 0) . '</td>
                 <td style="padding:10px; text-align:center;">' . number_format($totalCredit, 0) . '</td>
             </tr>';
