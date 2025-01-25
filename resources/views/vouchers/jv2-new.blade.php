f95567bcaebc45563f85e197b9f748e670ea6fcb
HamzaShaukat123 <162437543+HamzaShaukat123@users.noreply.github.com>
2025-01-25 17:11:33 +0500

    Update jv2-new.blade.php

--- resources/views/vouchers/jv2-new.blade.php
+++ resources/views/vouchers/jv2-new.blade.php
@@ -663,10 +663,8 @@
 
 			// Include prefix and pdc_id as hidden fields along with remarks
 			row += `<td>
-						<input type="text" name="prefix[]" value="${account['prefix'] || ''}">
-						<input type="text" name="pdc_id[]" value="${account['pdc_id'] || ''}">
-						<input type="text" class="form-control" name="remarks[]" value="${remarks || ''}">
-					</td>`;
+						<input type="text" class="form-control" name="remarks[]" value="${remarks || ''} ${prefix || ''} ${pdc_id || ''}">
+					</td>;
 			
 			row += `<td><input type="text" class="form-control" name="bank_name[]" value="${bankname || ''}"></td>`;
 			row += `<td><input type="text" class="form-control" name="instrumentnumber[]" value="${instrumentnumber || ''}"></td>`;
