2115866b5fadb16c3d696bf33c5549c696ea8f3f
HamzaShaukat123 <162437543+HamzaShaukat123@users.noreply.github.com>
2025-01-26 13:00:08 +0500

    Update JV2Controller.php

--- app/Http/Controllers/JV2Controller.php
+++ app/Http/Controllers/JV2Controller.php
@@ -115,6 +115,20 @@ class JV2Controller extends Controller
             }
         }
 
+
+        // is induced 1 for From Stock Out
+        if($request->has('isInduced') && $request->isInduced == 1){
+            $tstock_out = new tstock_out();
+
+            $SalinducedID=$pur_2_id['Sal_inv_no'];
+            $prefix=$pur_2_id['prefix'];
+            $pur_inv = $prefix.''.$SalinducedID;
+            $tstock_out->pur_inv = $pur_inv;
+            tstock_out::where('Sal_inv_no', $request->inducedID)->update([
+                'pur_inv'=>$tstock_out->pur_inv,
+            ]);
+        }   
+
         if($request->has('prevInvoices') && $request->prevInvoices!=0)
         {
             for($j=0;$j<$request->totalInvoices;$j++)
