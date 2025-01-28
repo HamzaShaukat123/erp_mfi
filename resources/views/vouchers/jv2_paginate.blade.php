69223deb00f53dc08a798aef0ed5fe358da701c7
HamzaShaukat123 <162437543+HamzaShaukat123@users.noreply.github.com>
2025-01-28 18:55:17 +0500

    Update jv2_paginate.blade.php

--- resources/views/vouchers/jv2_paginate.blade.php
+++ resources/views/vouchers/jv2_paginate.blade.php
@@ -9,7 +9,7 @@
                         <div class="col">
                             <section class="card">
                                 <header class="card-header" style="display: flex;justify-content: space-between;">
-                                    <h2 class="card-title">Journal Voucher 2 <Paginate> </h2>
+                                    <h2 class="card-title">Journal Voucher 2 (Paginate)</h2>
                                     <form class="text-end" action="{{ route('new-jv2') }}" method="GET">
                                         <button type="submit" class="btn btn-primary"> <i class="fas fa-plus"></i> New Voucher</button>
                                     </form>
