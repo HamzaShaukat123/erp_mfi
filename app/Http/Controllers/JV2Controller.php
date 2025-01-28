c6e80faca59d09d6f5cab4c6136215a6a38e21c1
HamzaShaukat123 <162437543+HamzaShaukat123@users.noreply.github.com>
2025-01-28 18:46:15 +0500

    Update JV2Controller.php

--- app/Http/Controllers/JV2Controller.php
+++ app/Http/Controllers/JV2Controller.php
@@ -28,37 +28,44 @@ class JV2Controller extends Controller
 
     public function index()
     {
-        // $jv2 = Lager0::where('lager0.status', 1)
-        //     ->select(
-        //         'lager0.jv_no',
-        //         'lager0.jv_date',
-        //         'lager0.narration',
-        //         DB::raw('COALESCE(dc.total_debit, 0) AS total_debit'),
-        //         DB::raw('COALESCE(dc.total_credit, 0) AS total_credit'),
-        //         DB::raw("GROUP_CONCAT(DISTINCT CONCAT(sales_ageing.sales_prefix, sales_ageing.sales_id) SEPARATOR ' ; ') AS merged_sales_ids"),
-        //         DB::raw("GROUP_CONCAT(DISTINCT CONCAT(purchase_ageing.sales_prefix, purchase_ageing.sales_id) SEPARATOR ' ; ') AS merged_purchase_ids"),
-        //         'sales_ageing.status as sales_status',
-        //         'purchase_ageing.status as purchase_status'
-        //     )
-        //     ->leftJoin(
-        //         DB::raw('(SELECT auto_lager, SUM(debit) AS total_debit, SUM(credit) AS total_credit FROM lager GROUP BY auto_lager) AS dc'),
-        //         'lager0.jv_no', '=', 'dc.auto_lager'
-        //     )
-        //     ->leftJoin('sales_ageing', 'lager0.jv_no', '=', 'sales_ageing.jv2_id')
-        //     ->leftJoin('purchase_ageing', 'lager0.jv_no', '=', 'purchase_ageing.jv2_id')
-        //     ->groupBy(
-        //         'lager0.jv_no',
-        //         'lager0.jv_date',
-        //         'lager0.narration',
-        //         'dc.total_debit', // Add total_debit to GROUP BY
-        //         'dc.total_credit', // Add total_credit to GROUP BY
-        //         'sales_ageing.status',
-        //         'purchase_ageing.status'
-        //     )
-        //     ->get();
+        $jv2 = Lager0::where('lager0.status', 1)
+            ->select(
+                'lager0.jv_no',
+                'lager0.jv_date',
+                'lager0.narration',
+                DB::raw('COALESCE(dc.total_debit, 0) AS total_debit'),
+                DB::raw('COALESCE(dc.total_credit, 0) AS total_credit'),
+                DB::raw("GROUP_CONCAT(DISTINCT CONCAT(sales_ageing.sales_prefix, sales_ageing.sales_id) SEPARATOR ' ; ') AS merged_sales_ids"),
+                DB::raw("GROUP_CONCAT(DISTINCT CONCAT(purchase_ageing.sales_prefix, purchase_ageing.sales_id) SEPARATOR ' ; ') AS merged_purchase_ids"),
+                'sales_ageing.status as sales_status',
+                'purchase_ageing.status as purchase_status'
+            )
+            ->leftJoin(
+                DB::raw('(SELECT auto_lager, SUM(debit) AS total_debit, SUM(credit) AS total_credit FROM lager GROUP BY auto_lager) AS dc'),
+                'lager0.jv_no', '=', 'dc.auto_lager'
+            )
+            ->leftJoin('sales_ageing', 'lager0.jv_no', '=', 'sales_ageing.jv2_id')
+            ->leftJoin('purchase_ageing', 'lager0.jv_no', '=', 'purchase_ageing.jv2_id')
+            ->groupBy(
+                'lager0.jv_no',
+                'lager0.jv_date',
+                'lager0.narration',
+                'dc.total_debit', // Add total_debit to GROUP BY
+                'dc.total_credit', // Add total_credit to GROUP BY
+                'sales_ageing.status',
+                'purchase_ageing.status'
+            )
+            ->get();
+
+           
+
+        return view('vouchers.jv2', compact('jv2'));
+
+    }
 
 
-
+    public function index_paginate()
+    {
         $jv2 = Lager0::where('lager0.status', 1)
             ->select(
                 'lager0.jv_no',
@@ -86,8 +93,8 @@ class JV2Controller extends Controller
                 'sales_ageing.status',
                 'purchase_ageing.status'
             )
-        ->orderBy('lager0.jv_no', 'desc') // Order by jv_date in descending order
-        ->paginate(75); // This will paginate the last 10 records
+            ->orderBy('lager0.jv_no', 'desc') // Order by jv_date in descending order
+            ->paginate(100); // This will paginate the last 100 records
 
            
 
