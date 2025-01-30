8f58005c1e5b534654b32600dc136ca21f062c7c
HamzaShaukat123 <162437543+HamzaShaukat123@users.noreply.github.com>
2025-01-30 17:09:49 +0500

    Update jv2-new.blade.php

--- resources/views/vouchers/jv2-new.blade.php
+++ resources/views/vouchers/jv2-new.blade.php
@@ -665,7 +665,7 @@
 			}
 
 			row += `<td>
-						<input type="text" name="pdc_id[]" value="${account['pdc_id'] || ''}">
+						<input type="hidden" name="pdc_id[]" value="${account['pdc_id'] || ''}">
 						<input type="text" class="form-control" name="remarks[]" value="${remarks || ''} ${account['prefix'] || ''} ${account['pdc_id'] || ''}">
 					</td>`;
 
