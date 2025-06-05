d425805c7b68c7d1f8f14070dd867df4271bc8d7
HamzaShaukat123 <162437543+HamzaShaukat123@users.noreply.github.com>
2025-06-05 14:05:15 +0500

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1762,7 +1762,7 @@
 		}
 
 		// Graph Chart for MILL WISE HR PIPE PURCHASE Started
-		const mills = ['296', '82', '73','86', '88'];
+		const mills = ['296', '82', '73','86','88'];
 		const IILmills = ['1', '5', '6', '7', '8']; // Mill codes to include
 
 		const colors = [
@@ -1771,6 +1771,7 @@
 			'rgba(25, 135, 84, 1)',
 			'rgba(219, 150, 81, 1)',
 			'rgba(43, 170, 177, 1)',
+			'rgba(255, 105, 180, 1)',
 			
 			
 		];
